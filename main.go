70ad8cc2948dc9380ef3f55ccd6117d1385ffad8
kenstalks <77483349+kentalks@users.noreply.github.com>
2024-02-07 16:13:41 +0800

    20240207-1

--- main.go
+++ main.go
@@ -3,14 +3,34 @@ package main
 import (
 	"math/rand"
 	"time"
+
+	"kensoft.tech/go-study/function"
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+
 }
 
 func main() {
-	// function.PrintHellos([]string{"mei", "ken"})
+	function.HelloSlice()
+
+	//function.HelloArray()
+
+	/*
+		cat := function.Cat{
+			Age: 0,
+			Pet: &function.Pet{Name: "小白"},
+		}
+		function.Super(&cat)
+		(&cat).Super()
+		cat.Super()
+		cat.Info()
+		fmt.Println(cat.Pet)
+	*/
+
+	//function.PrintHellos([]string{"mei", "ken"})
 
 	// r := function.RandString(10)
 	// fmt.Println(r)
@@ -29,6 +49,10 @@ func main() {
 			out = append(out, &v)
 		}
 		fmt.Println(*out[0], *out[1], *out[2])
+	*/
+
+	/*
+		fmt.Println(function.GCD(-6, 18))
 	*/
 
 }
